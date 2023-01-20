05cd7fe088156ac8234e91b68eb38d2f15fcfdde
EversonDias <eversond14s@gmail.com>
2023-01-20 16:43:28 -0300

    I created the home test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do App', () => {
+  it('no topo da aplicação deve conter um link Home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    expect(home).toBeInTheDocument();
+  });
+});
